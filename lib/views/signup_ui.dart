6f6130d4bc652f2308c76f30ea44e8ecc7d661d0
ThamKub <s6452410012@sau.ac.th>
2023-09-06 16:47:17 +0700

    Build Awesome V.2

--- lib/views/signup_ui.dart
+++ lib/views/signup_ui.dart
@@ -1,4 +1,4 @@
-// ignore_for_file: prefer_const_constructors
+// ignore_for_file: prefer_const_constructors, sort_child_properties_last
 
 import 'package:build_awesome_app/views/login_ui.dart';
 import 'package:flutter/material.dart';
